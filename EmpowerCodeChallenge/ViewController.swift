d042bdbf978b2f2c1b76e41e52b182150fdbdd98
Mike Phan <khoaphan90@gmail.com>
2024-03-11 15:23:38 -0400

    Initial Commit

---
+++ EmpowerCodeChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmpowerCodeChallenge
+//
+//  Created by Mike Phan on 3/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
